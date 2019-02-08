cb890b17e09b2e4270ced50c8f17d07d125c15f5
Bruce Bookman <bruce.bookman@gmail.com>
2019-02-07 20:18:32 -0800

    more constants, more tests

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,23 @@
+#TODO Remove hardcoded keywords if UI happens
+KEYWORDS = [
+
+'bash', 'python', 'java', 'c++', 'ruby', 'perl', 'matlab', 'javascript', 'scala', 'firmware'
+'php', 'Sauce Labs', 'flask', 'shell', 'Telecom', 'NAS', 'SAN', 'iSCSI', 'scripts', 'scripting',
+'junit', 'selenium', 'react', 'c#', 'TestRail', 'Confluence', 'JMeter',
+'tableau', 'd3.js', 'sas', 'spss', 'd3', 'saas', 'pandas', 'numpy', 'Jenkins', 'scipy', 'plan', 'case',
+'sps', 'spotfire', 'scikits.learn', 'splunk', 'h2o', 'jira', 'functional', 'integration', 'stress', 'load', 'performance',
+'hadoop', 'mapreduce', 'spark', 'pig', 'hive', 'shark', 'oozie', 'zookeeper', 'flume', 'mahout',
+'elasticsearch', 'api', 'Mockito', 'Robotium', 'frontend', 'backend',
+'sql', 'nosql', 'hbase', 'cassandra', 'xml', 'rust', 'mongodb', 'mysql', 'mssql', 'postgre', 'oracle',
+'rdbms', 'mobile', 'android', 'ios', 'cucumber', 'iot', 'black', 'white', 'telecommunications',
+'hive', 'cucumber', 'aws', 'azure', 'amazon', 'google', 'rest', 'docker', 'container', 'puppet', 'chef',
+'kubernetes', 'storage', 'network', 'networking', 'maven', 'ci', 'cd', 'ci/cd', 'gui', 'ui', 'scrapy', 'beautifulsoup',
+'restassured', 'ios', 'json', 'swift', 'objective-c', 'groovy', '.net', 'angular', 'node.js', 'kafka', 'mesos',
+'django', 'pytest', 'css', 'html', 'appium', 'linux', 'css', 'ui', 'soa', 'unix', 'RESTful', 'Elastic', 'git', 'github', 'database',
+'acceptance', 'uat', 'healthcare', 'banking', 'rest',
+]
+
+
 
 
 
@@ -5,10 +25,19 @@
  https://www.indeed.com/jobs?as_and=senior+technical+support+engineer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=fulltime&st=&as_src=&salary=145000&radius=100&l=San+Jose%2C+CA&fromage=15&limit=50&sort=&psf=advsrch
 
 '''
-INDEED_ROOT_URL = 'http://indeed.com/jobs?'
+INDEED_ROOT_URL = 'https://www.indeed.com/jobs?'
 INDEED_TITLE_QUERY_TERM = 'as_and'
 INDEED_JOB_TYPE_QUERY_TERM = 'jt'  #fulltime, parttime . . .
 INDEED_SALARY_QUERY_TERM = 'salary'
 INDEED_DISTANCE_QUERY_TERM = 'radius'
 INDEED_LOCATION_QUERY_TERM = 'l'
 INDEED_JOB_DESCRIPTION_AGE_QUERY_TERM = 'fromage'
+INDEED_LIST_LIMIT_QUERY_TERM = 'limit'
+INDEED_ADVANCED_SERCH_INDICATOR = 'psf=advsrch'
+INDEED_URL_TEMPATE = INDEED_ROOT_URL + INDEED_TITLE_QUERY_TERM + '={}&' + INDEED_JOB_TYPE_QUERY_TERM + '={}&' + INDEED_SALARY_QUERY_TERM + '={}&' + INDEED_DISTANCE_QUERY_TERM + '={}&' + INDEED_LOCATION_QUERY_TERM + '={}&' +  INDEED_JOB_DESCRIPTION_AGE_QUERY_TERM + '={}&' + INDEED_LIST_LIMIT_QUERY_TERM + '={}&' + INDEED_ADVANCED_SERCH_INDICATOR
+
+
+
+
+
+
--- src/tests.py
+++ src/tests.py
@@ -6,7 +6,7 @@ def test_site_root_urls_exist():
     assert isinstance(INDEED_ROOT_URL, str)
 
 
-def test_site_title_query_terms_exists():
+def test_site_query_terms_exists():
     expected = [
         INDEED_TITLE_QUERY_TERM,
         INDEED_JOB_TYPE_QUERY_TERM,
@@ -18,3 +18,19 @@ def test_site_title_query_terms_exists():
     is_string = [True for item in expected if isinstance(item, str)]
     assert expected
     assert all(is_string)
+
+def test_site_template_url():
+    assert INDEED_URL_TEMPATE
+    assert isinstance(INDEED_URL_TEMPATE, str)
+    expected = 'https://www.indeed.com/jobs?as_and=TITLE&jt=FULLTIME&salary=100000&radius=100&l=LOCATION&fromage=AGE&limit=LIMIT&psf=advsrch'
+    test_url = INDEED_URL_TEMPATE.format('TITLE', 'FULLTIME', '100000', '100', 'LOCATION', 'AGE', 'LIMIT')
+    assert expected == test_url
+
+#TODO Remove hardcoded keywords if UI happens
+
+def test_keywords():
+    assert KEYWORDS
+    assert isinstance(KEYWORDS, list)
+    is_string = [True for keyword in KEYWORDS if isinstance(keyword, str)]
+    assert is_string
+
