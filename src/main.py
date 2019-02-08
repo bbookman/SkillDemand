f9d30ac7bdc3e67991731d59a6f8ad477159daef
Bruce Bookman <bruce.bookman@gmail.com>
2019-02-08 15:08:13 -0800

    function for parse site for jd links works

--- src/main.py
+++ src/main.py
@@ -3,8 +3,23 @@ from bs4 import BeautifulSoup as beautiful
 from constants import *
 
 
+def parse_site_for_jd_links(url, link_finders):
+    """
+    :param url:
+    :return: links:
+    """
+    page = urllib2.urlopen(url)
+    soup = beautiful(page, 'html.parser')
+    links = soup(link_finders[0], link_finders[1])
+    str_links =  [str(link) for link in links]
+    return str_links
 
 
+
+
+
+
+'''
 url = 'https://www.indeed.com/jobs?as_and=senior+technical+support+engineer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=fulltime&st=&as_src=&salary=145000&radius=100&l=San+Jose,+CA&fromage=300&limit=50&sort=&psf=advsrch'
 page = urllib2.urlopen(url)
 soup = beautiful(page, 'html.parser')
@@ -16,9 +31,6 @@ for item in parse_me:
     start_title_loc = item.find('title=')
     end_title_loc = item.find('">')
     title = item[  start_title_loc + 7: end_title_loc]
-
-
-'''
     href_loc = item.find('href')
     close = item.find('>')
     href = item[href_loc + 6:close]
@@ -28,7 +40,6 @@ for item in parse_me:
 title="Senior QA Engineer II">
 
 <b>Senior</b> QA <b>Engineer</b> II</a>
-
 
 
 
