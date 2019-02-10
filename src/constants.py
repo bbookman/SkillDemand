961fc9c2b1d84747a5112986bdf4c931c910807a
Unknown <bruce.bookman@gmail.com>
2019-02-09 21:09:30 -0800

    remove constant thresh

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,5 @@
 SUPERFLOUS_STRINGS = ['senior', 'director', 'manager', 'lead', 'mobile', 'sr', 'jr', 'I', 'II', 'III', 'IV','V' ,'(', ')', '.', ',', '/', '\\', "\'", '\"', '-',]
 
-TITLE_MATCH_THRESHOLD = 90
-
 INDEED_URL_TEMPATE = 'https://www.indeed.com/jobs?as_and={}&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt={}&st=&as_src=&salary={}&radius={}&l={}fromage={}&limit=500&sort=&psf=advsrch'
 # https://www.indeed.com/jobs?as_and=senior+technical+support+engineer&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=fulltime&st=&as_src=&salary=145000&radius=100&l=San+Jose,+CA&fromage=300&limit=50&sort=&psf=advsrch
 INDEED_TITLE_ONLY_TEMPLATE = 'https://www.indeed.com/q-{}.html' #dash separated title text such as data-science-engineer
