a03a7a7c4baa8d5963ef10c9e412334669352ba0
Unknown <bruce.bookman@gmail.com>
2019-02-25 17:36:29 -0800

    no longer broken

--- src/main.py
+++ src/main.py
@@ -127,17 +127,13 @@ def get_bodies(site_id, site_url_template, title, title_separator, title_selecto
     browser = _start_driver()
     new_tab = _start_driver()
     job_title = _build_job_title(title, title_separator)
-    loop_count = 0 #todo remove
-    for page in range(1,7):
+    for page in range(1):
         if site_id == 'indeed':
             url = _build_site_url( site_id, site_url_template, job_title, salary, zip, radius, age,)
-            loop_count+=1
-            print(f'THIS LOOPED {loop_count}')
         if site_id == 'careerbuilder':
             url = _build_site_url( site_id, site_url_template, title, salary, zip, radius, age,)
-            url += f'page={page}'
+            url += f'page_number={page}'
         browser.get(url)
-        logging.info(f'get: {url}')
         print("----------------------------------------------")
         print(f'title:{job_title.upper()}')
         print(f'Page:{page}')
@@ -211,24 +207,26 @@ def get_bodies(site_id, site_url_template, title, title_separator, title_selecto
 results = dict()
 location = dict()
 income = dict()
-zcode = dict()
+
 
 geo = 'San Francisco Bay Area'
+results.setdefault(geo, 'San Francisco Bay Area')
 
 jobtitle = 'software quality assurance engineer'
 skilllist = SKILL_KEYWORDS_QA
+title_dict = {'software': 50, 'quality': 60, 'assurance': 30, 'qa': 80, 'sqa': 90, 'sdet': 100, 'test': 70, 'automation': 70, 'engineer': 20}
+threshold = 90
 
-
+'''
 site_id = 'indeed'
 title_separator = SITES_DICT[site_id]['title_word_sep']
 title_selector = SITES_DICT[site_id]['title_selector']
-salaries = ['50000', '75000',  '100000', '150000', '200000']
-title_dict = {'software': 50, 'quality': 60, 'assurance': 30, 'qa': 80, 'sqa': 90, 'sdet': 100, 'test': 70, 'automation': 70, 'engineer': 20}
-threshold = 90
+salaries = ['50000', '100000', '150000']
+
 site_url_template = SITES_DICT[site_id]['url_template']
 zips = SF_ZIPS
 
-'''
+
 print(f'THIS GEO {geo} ')
 for salary in salaries:
     print(f'THIS SALARY {salary} ')
@@ -248,28 +246,42 @@ with open('indeedRESULTS.txt', 'w') as file:
 site_id = 'careerbuilder'
 title_separator = SITES_DICT[site_id]['title_word_sep']
 title_selector = SITES_DICT[site_id]['title_selector']
-salaries = ['50', '75', '100', '150', '200']
+salaries = ['50', '100', '150']
 site_url_template = SITES_DICT[site_id]['url_template']
 zips = SF_ZIPS
 
 print(f'THIS GEO {geo} ')
 for salary in salaries:
     print(f'THIS SALARY {salary} ')
+    zcode = dict()
     for zip in zips:
+       # if zip in zcode:
+        #    import pdb; pdb.set_trace()
         print(f'THIS ZIP {zip}')
         skill_counts = get_bodies(site_id, site_url_template, jobtitle, title_separator, title_selector, salary,
                                   skilllist, title_dict, threshold, radius='30', age='60')
         zcode[zip] = skill_counts
-    salary = salary + '000'
     income[salary] = zcode
 location[geo] = income
+logging.info(f'location:{location}')
 results[jobtitle] = location
 
 with open('cbRESULTS.txt', 'w') as file:
     file.write(str(results))
 
 
+'''
+{'software quality assurance engineer': {'San Francisco Bay Area': 
 
+['Title']:
+    ['Geo']:
+        ['Salary']
+            ['Zip']
+                ['Skill']:count
+            ['Zip]
+                 ['Skill']:count
+                
+'''
 
 
 
